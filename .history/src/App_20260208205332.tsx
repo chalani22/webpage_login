eda2e645a225650e2ce68724d1a08e705105950d
Chalani_Nadeesha <chalaninadeesha2345@gmail.com>
2026-02-08 21:04:18 +0530

    add app routing for login and token pages

---
+++ .history/src/App_20260208205332.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router";
+
+export default function App() {
+  return (
+    <>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </>
+  );
+}
